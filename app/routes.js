87d393e73ce0aa2b65baa5d6875fd1ec0c10a191
Tarun Sharma <tarun.softengg@gmail.com>
2017-04-17 22:55:01 +0530

    commit rollbacjk

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,7 @@ let routes = function(app) {
     // user service routes
     app.use('/users', ValidAuthTokenMiddleware, UsersServiceRoutes);
     // user training routes
-    app.use('/training', ValidAuthTokenMiddleware, TrainingServiceRoutes);
+    app.use('/training', TrainingServiceRoutes);
     app.use('/webinar', ValidAuthTokenMiddleware, WebinarServiceRoutes);
     app.use('/payment', ValidAuthTokenMiddleware, PaymentServiceRoutes);
     app.use('/registration', ValidAuthTokenMiddleware, RegisterServiceRoutes);
