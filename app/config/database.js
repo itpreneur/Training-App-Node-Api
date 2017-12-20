eee30e646cf2b23b28d271276c9c4b077367f558
Tarun <Tarun.softengg@gmail.com>
2017-12-20 23:10:23 +0530

    client callback updated

--- app/config/database.js
+++ app/config/database.js
@@ -3,7 +3,7 @@
 export default {
 
 	default: {
-		//HOST: 'mongodb://localhost:27017/gennext',
+		//HOST: 'mongodb://heroku_hxz5c5g5:uuldp1gfq7p6knielv4rdbf0rf@ds153501.mlab.com:53501/heroku_hxz5c5g5'
 		HOST : process.env.MONGODB_URI
 	}
 }
--- app/config/frontend.js
+++ app/config/frontend.js
@@ -1,5 +1,6 @@
 'use strict';
 
 export default {
-	URL: 'http://localhost:8080'
+   //URL: 'http://localhost:8080'
+	URL: 'https://hackio.herokuapp.com'
 }
